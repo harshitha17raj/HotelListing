865ea06e5f906d5d543ded5b1827582382bab1b0
harshitha17raj <harshitharajendhran@gmail.com>
2018-12-28 15:37:30 +0530

    acc

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,88 @@
+class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:edit, :update, :delete]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:index, :destroy]
+
+  def show
+    @user = User.find(params[:id])
+    if @user == current_user
+	render 'show'
+    elsif current_user.try(:admin?)
+	render 'show'
+    else
+	redirect_to root_url
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+     redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+  # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+  # Confirms an admin user.
+    def admin_user
+     if current_user.nil?
+       redirect_to login_url
+     elsif !(current_user.try(:admin?))
+       redirect_to root_url
+   end
+  end
+  
+end
